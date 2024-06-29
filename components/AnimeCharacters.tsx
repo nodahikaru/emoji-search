9519c575acedef5197e175c0d16eef2888ca2747
Noda Hikaru <like365.hondai@gmail.com>
2024-06-30 07:44:54 +0900

    added list

--- components/AnimeCharacters.tsx
+++ components/AnimeCharacters.tsx
@@ -1,10 +1,23 @@
+'use client'
+import useSWR from 'swr'
 import AnimeCharacter from "./animeCharacter";
+import { useEffect } from 'react';
 
+const fetcher = (arg : any, ...args:any) => fetch(arg, ...args).then((res) => res.json())
 
 export default function AnimeCharacters() {
+  
+  const { data, error } = useSWR('https://api.nekosapi.com/v3/characters', fetcher);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data])
+
   return (
     <div className="flex items-center justify-center flex-wrap py-6">
-      <AnimeCharacter />
+      { data.items.map((item : any) => (
+        <AnimeCharacter {...item} />
+      )) }
     </div>
   )
 }
--- components/animeCharacter.tsx
+++ components/animeCharacter.tsx
@@ -1,17 +1,33 @@
-export default function AnimeCharacter() {
+export default function AnimeCharacter({...args}: AnimeCharacter) {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <a className="max-w-sm h-80 mx-4 my-4 rounded overflow-hidden shadow-lg flex flex-col justify-between cursor-pointer">
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <div className="font-bold text-xl mb-2">{args.name}</div>
         <p className="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          {args.description}
         </p>
       </div>
       <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{args.nationality}</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{args.gender}</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{args.species}</span>
       </div>
-    </div>
+    </a>
   )
+}
+
+export interface AnimeCharacter {
+  id: number,
+  id_v2: string,
+  name: string,
+  aliases: string[],
+  description: string,
+  ages: number[],
+  height?: number,
+  weight?: number,
+  gender: string,
+  species: string,
+  birthday: string,
+  nationality: string,
+  occupations: string[]
 }
