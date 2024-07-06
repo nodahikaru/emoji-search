8c2ef896b0b69f53049380616af1f24795e3d596
Noda Hikaru <like365.hondai@gmail.com>
2024-07-06 20:21:40 +0900

    updated

--- app/anime/[slug].tsx
+++
@@ -1,7 +0,0 @@
-export default function AnimeDetail() {
-  return (
-    <main className="flex bg-white min-h-screen flex-col items-center p-24">
-      
-    </main>
-  )
-}
--- app/anime/page.tsx
+++ app/anime/page.tsx
@@ -1,4 +1,3 @@
-import AnimeArtists from "@/components/animeArtists";
 import AnimeCharacters from "@/components/AnimeCharacters";
 import AnimeTags from "@/components/animeTags";
 
@@ -7,7 +6,6 @@ export default function Anime() {
   return (
     <main className="flex bg-white min-h-screen flex-col items-center p-24">
       <AnimeTags />
-      <AnimeArtists />
       <AnimeCharacters />
     </main>
   )
