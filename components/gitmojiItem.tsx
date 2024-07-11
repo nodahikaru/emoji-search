706c40dbe8f5ca06d8ca69a9d1ea2329ba91f53b
Noda Hikaru <like365.hondai@gmail.com>
2024-07-11 09:34:19 +0900

    added toast

--- components/gitmojiItem.tsx
+++ components/gitmojiItem.tsx
@@ -1,3 +1,5 @@
+import { toast } from 'react-hot-toast';
+
 export interface GitmojiItemType {
   emoji: string;
   entity?: string;
@@ -9,8 +11,12 @@ export interface GitmojiItemType {
 
 export default function GitmojiItem({ emoji, description, name } : GitmojiItemType) {
 
+  const onClick = () => {
+    toast.success('You did it');
+  }
+
   return (
-    <div className="flex flex-col items-center p-4 shadow hover:shadow-md cursor-pointer m-2 rounded-md">
+    <div className="flex flex-col items-center p-4 shadow hover:shadow-md cursor-pointer m-2 rounded-md" onClick={onClick}>
       <h1 className="text-[32px]">{emoji}</h1>
       <h1 className="font-bold">:{name}</h1>
       <div className="text-[12px] py-2 max-w-32 text-center overflow-hidden">{description}</div>
