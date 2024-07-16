b4aedf2c40957547485472cd431f1b4556b06f36
Noda Hikaru <like365.hondai@gmail.com>
2024-07-16 20:02:34 +0900

    updated

--- components/gitmojiHeader.tsx
+++ components/gitmojiHeader.tsx
@@ -10,12 +10,6 @@ const BlueLink = styled.a `
 export default function GitmojiHeader() {
   return (
     <>
-      <h1>
-        Gitmoji Search <span role="img" aria-label="Unicorn Emoji">🍝</span>
-      </h1>
-      <p>
-        A simple Gitmoji search tool made with <Link href="https://nextjs.org/" passHref legacyBehavior><BlueLink>Next.js</BlueLink></Link>
-      </p>
       <p className="max-w-[560px] text-center mt-4">
        <strong>Using emojis</strong> on <strong>commit messages</strong> provides an <strong>easy way</strong> of <strong>identifying the purpose or intention of a commit</strong> with only looking at the emojis used. As there are a lot of different emojis I found the need of creating a guide that can help to use emojis easier.
       </p>
