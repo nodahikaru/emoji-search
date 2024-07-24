73178200403714e3a26d4f0b2eba17a9ffc3aed2
Noda Hikaru <like365.hondai@gmail.com>
2024-07-25 00:55:25 +0900

    updated

--- components/blogItem.tsx
+++ components/blogItem.tsx
@@ -4,8 +4,8 @@ export default function BlogItem({ id, title, description, imageUrl } : BlogItem
   return (
     <div className="shadow hover:shadow-lg scale-100 hover:scale-110 transition-all rounded-md flex overflow-hidden flex-col items-start m-4 w-80 pb-2 cursor-pointer">
       <Image src={imageUrl} width="320" height="200" alt={title} />
-      <h3 className='p-2 font-bold text-lg'>{title}</h3>
-      <p className='px-2 text-sm h-24 text-ellipsis overflow-hidden'>{description}</p>
+      <h3 className='p-4 font-bold text-lg'>{title}</h3>
+      <p className='px-4 text-sm h-24 text-ellipsis overflow-hidden'>{description}</p>
     </div>
   )
 }
