3130de37d8190700a7a09bf10d992e156e0a2999
Noda Hikaru <like365.hondai@gmail.com>
2024-07-28 21:36:46 +0900

    updated

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,13 +1,28 @@
 'use client'
 import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import Image from 'next/image';
+import blogs from '@/data/blogs.json';
+import { BlogItemType } from "@/components/blogItem";
 
 export default function BlogDetail() {
   const params = useParams();
   const { slug } = params;
+
+const [ blog, setBlog ] = useState<BlogItemType | undefined>(undefined);
+
+  useEffect(() => {
+    const blog = blogs.find((item) => item.id == Number(slug))
+    setBlog(blog);
+  }, [slug])
   
   return (
     <main className="flex bg-white min-h-screen flex-col items-center p-24">
-      
+      {blog && <div className="w-full max-w-42 flex flex-col">
+        <div className="w-full">
+          <Image src={blog.imageUrl} fill objectFit="contain" alt={blog.title}/>
+        </div>
+        </div>}
     </main>
   )
 }
