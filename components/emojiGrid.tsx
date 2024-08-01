dcb9fa0447386ebe0014ce05e4dd1b488ee89f16
Noda Hikaru <like365.hondai@gmail.com>
2024-08-01 17:07:50 +0900

    updated

--- components/emojiGrid.tsx
+++ components/emojiGrid.tsx
@@ -4,12 +4,14 @@ import EmojiItem from './emojiItem';
 export default function EmojiGrid() {
   return (
     <div className="flex items-center justify-center flex-wrap py-6">
+      <div className="md:container md:mx-auto">
       { data.map((item, index) => (
         <EmojiItem 
           key={index} 
           symbol={item.symbol} 
         />
       )) }
+      </div>
     </div>
   )
 }
