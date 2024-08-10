d63c0ad8b16f0cd8b769f5e817e45174af3cd1c7
Noda Hikaru <like365.hondai@gmail.com>
2024-08-10 18:10:32 +0900

    updated

--- app/emoji/page.tsx
+++ app/emoji/page.tsx
@@ -4,6 +4,7 @@ import EmojiHeader from "@/components/emojiHeader";
 export default function Emoji() {
   return (
     <main className="flex bg-white min-h-screen flex-col items-center p-24">
+      <p>My Blog list </p>
       <EmojiHeader />
       <EmojiGrid />
     </main>
